9068c6edb2950247221aab731bcffb05227b9072
Ken Steckler <steckler92ken@gmail.com>
2022-05-24 22:40:20 -0700

    tweaked about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,24 +6,16 @@ const About = () => {
       <div class="container">
         <div class="card-body">
           <div class="container-body">
-            <h3>Hey there!</h3>
+            <h3>Hello!</h3>
             <p>
               I'm Ken - a software engineer interested in fullstack web
               development. I've worked with both the frontend and backend and
-              enjoy creating applications with my users in mind.
-            </p>
-            <p>
-              I'm always thinking of new things to create. I'm currently working
-              on a React meal prep app that will let me quickly decide what I
-              want to make for the week. I also plan on building my original tip
-              calculator app that lets me quickly calculate tip on my phone
-              without having to use my native calculator app.
+              enjoy creating apps.
             </p>
             <p>
               I am currently pursuing a Bacherlor's of Science degree in
               Computer Science at Oregon State University to build a
-              foundational knowledge in the world of software engineering so
-              that I can build better apps.
+              foundational knowledge in the software development world.
             </p>
             <p>
               I always strive to become a better version of myself. I try to
@@ -31,7 +23,6 @@ const About = () => {
               how I learn best. A fun fact about me, I'm also a licensed
               pharmacist! I also enjoy jogging and photography.
             </p>
-            <p>Thank you for reading and I hope you have a wonderful day!</p>
           </div>
         </div>
       </div>
